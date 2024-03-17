de4df2ba843583a7b0ebe5485a41c657ab653388
Laksh Heera <lakshheera72@gmail.com>
2024-03-17 10:51:57 +0530

    first commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const options = {
     params: { hl: "en", gl: "US" },
     headers: {
         "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+            process.env.REACT_APP_YOUTUBE_API_KEY || "a072f696bamshc80ad9b406cab29p17ca05jsn615792d3dcd0",
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
